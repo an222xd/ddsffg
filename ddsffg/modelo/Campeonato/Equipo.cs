16db29306162bf08e2d7af25533bff266138eb74
number2 <joseantonio222xd@gmail.com>
2019-03-07 13:47:04 -0600

    Modelo campeonato

---
+++ ddsffg/modelo/Campeonato/Equipo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ddsffg.modelo.Campeonato
+{
+	class Equipo
+	{
+		private int id;
+		private string nombre;
+		private string comunidad;
+		private string entrenador;
+
+		public Equipo()
+		{
+			this.nombre = "Deportivo 'EL MERO MERO' ";
+		}
+		public int Id
+		{
+			get
+			{
+				return this.id;
+			}
+			set
+			{
+				if (value > 0)
+				{
+					this.id = value;
+				}  
+			}
+		}
+
+
+	}
+}
---
+++ ddsffg/modelo/Campeonato/Jugador.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ddsffg.modelo.Campeonato
+{
+	class Jugador
+	{
+		private int id;
+		private string nombre;
+		private string apelllidos;
+		private DateTime fechaNacimiento;
+		private byte numero;
+		public Jugador()
+		{
+			this.nombre = "Papu Lince";
+			this.apelllidos = "Del campo de la via lactea";
+			this.fechaNacimiento = DateTime.Today;
+
+		}
+
+		public int Id { get; set; }
+		public string Nombre { get; set; }
+		public string Apellidos { get; set; }
+		public string Numero { get; set; }
+		public DateTime Fecha { get; set; }
+	}
+}
