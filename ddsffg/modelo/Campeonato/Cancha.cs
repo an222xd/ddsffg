c9d3d56064dff491be41e4d0cbb65b2c4b2bb73c
number2 <joseantonio222xd@gmail.com>
2019-03-07 13:12:30 -0600

    cambios 7/03/19

---
+++ ddsffg/modelo/Campeonato/Cancha.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ddsffg.modelo.Campeonato
+{
+	class Cancha
+	{
+		private int id;
+		private string nombre;
+		private string ubicacion;
+		public int Id
+		{
+			get
+			{
+				return this.id;
+			}
+			set
+			{
+				if (value > 0)
+				{
+					this.id = value;
+				}
+			}
+
+		}
+		public string Nom
+		{
+			get
+			{
+				return this.nombre;
+			}
+			set
+			{
+				if(value != null || (value.Length > 2 && value.Length <= 30))
+				{
+					this.nombre = value;
+				}
+			}
+		}
+		public string ubi
+		{
+			get
+			{
+				return this.nombre;
+			}
+			set
+			{
+
+			}
+		}
+	}
+
+}
