aa0793e5395e7d2c7bb702587a6969e0d1613b3d
number2 <joseantonio222xd@gmail.com>
2019-03-08 11:27:28 -0600

    Nueva Mejora

---
+++ ddsffg/modelo/Campeonato/Jornada.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ddsffg.modelo.Campeonato
+{
+	class Jornada
+	{
+		private int id;
+		private string nombre;
+		private DateTime fechaInicio;
+		private DateTime fechaFin;
+		private bool isConcluida;
+
+		public Jornada()
+		{
+			this.fechaInicio = DateTime.Today;
+			this.isConcluida = false;
+			
+		}
+		public int Id
+		{
+			get
+			{
+				return this.id;
+			}
+			set
+			{
+				if (value > 0)
+				{
+					this.id = value;
+				}
+			}
+		}
+		public string Nombre
+		{
+			get
+			{
+				return this.nombre;
+
+			}
+			set
+			{
+				{
+					if (value != null || (value.Length >= 3 && value.Length <= 30))
+					{
+						this.nombre = value;
+					}
+				}
+			}
+		}
+		public DateTime FechaInicio
+		{
+			get
+			{ return this.fechaInicio; }
+			set
+			{
+				if (value > DateTime.Today)
+				{
+					this.fechaInicio = value;
+				}
+			}
+		}
+		public DateTime FechaFin
+		{
+			get
+			{ return this.FechaFin; }
+			set
+			{
+				DateTime fin = new DateTime(2019, 4, 8);
+				if (value > DateTime.Today)
+				{
+					this.fechaFin = value;
+				}
+			}
+		}
+		public bool IsConcluida { get; set; }
+	}
+
+}
---
+++ ddsffg/modelo/Campeonato/campeonato1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ddsffg.modelo.Campeonato
+{
+	class campeonato1
+	{
+
+		private int id;
+		private string nombre;
+		private DateTime fechaInicio;
+		private DateTime fechaFin;
+
+		//Constructor default
+		public campeonato1()
+		{
+			this.nombre = "Campeonato de la segunda divicion Doleondres";
+			this.id = 1;
+			for (int i = 1; i <= 20; i++)
+			{
+				Jornada jornada1 = new Jornada();
+				jornadas.Add(jornada1);
+			}
+		}
+		//Resultado de la asociacion entre campeonato y jornada
+		private List<Jornada> jornadas = new List<Jornada>();
+
+		public int Id { get; set; }
+		public string Nombre { get; set; }
+		public DateTime FechaInicio { get; set; }
+		public DateTime FecaFin { get; set; }
+
+	}
+}
