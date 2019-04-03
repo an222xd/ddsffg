24346d2151b86b68330f53b0d94b9402134d8e52
number2 <joseantonio222xd@gmail.com>
2019-04-03 10:45:26 -0600

    mejora

--- ddsffg/Program.cs
+++ ddsffg/Program.cs
@@ -21,7 +21,7 @@ namespace ddsffg
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Principal());
+            Application.Run(new Principal()); 
         }
     }
 }
